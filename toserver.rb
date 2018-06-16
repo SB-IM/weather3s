271d2ecd12c2398437d2d770d08c3e02da1f8090
a-wing <shiyongxin@aliyun.com>
2018-06-16 14:40:54 +0800

    Fix http.get err

--- toserver.rb
+++ toserver.rb
@@ -1,11 +1,20 @@
 require 'socket'
 require 'net/http'
 
+remote = ARGV[0] || "localhost"
+
 socket = TCPSocket.new 'localhost', 8900
 
 while line = socket.gets.delete("\r\n")
   puts line if line.length == 72
-  Net::HTTP.get('localhost', "/put/#{line}", '1207') if line.length == 72
+
+  begin
+    Net::HTTP.get(remote, "/put/#{line}", '1207') if line.length == 72
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.inspect
+  end
+
 end
 
 s.close
