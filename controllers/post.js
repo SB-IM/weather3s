4b6975b5fb75199d92af6245afc473c92e0e9096
a-wing <shiyongxin@aliyun.com>
2018-07-03 11:50:06 +0800

    Add hmac signture

--- controllers/post.js
+++ controllers/post.js
@@ -1,37 +1,51 @@
 const weather = require('../models/weather')
 
 module.exports = function (req, res) {
-  const data = req.params.data.split(/[A-Z]/)
-  w = new weather({
-    wind_direction_AD: data[1],
-    wind_direction: data[2],
-    wind_speed_Hz: data[3],
-    wind_speed: data[4],
-    wind_speed_before_1m: data[5],
-    wind_speed_before_5m: data[6],
-    rainfall_count: data[7],
-    rainfall_count_before_1m: data[8],
-    rainfall_before_1m: data[9],
-    rainfall_before_1h: data[10],
-    rainfall_before_1d: data[11],
-    temperature: data[12],
-    humidity: data[13],
-    air_pressure: data[14],
-    timestamp: Date.now()
-  })
-  //console.log("write database")
-  //console.log(req.params.data)
-  //console.log(w)
-  w.save(function (err, d) {
-    if (err) {
-      //logger.error(err);
-      //res.send(`{"code": 0, "msg": "Error happens, please contact system administrator."}`);
-      res.json({ msg: "Error happens, please contact system administrator." })
-    }
-    else {
-      console.log("save successed")
-      res.json({ msg: "save successed" })
-    }
-  })
-        //  res.json({ user: 'tobi' })
+
+  //console.log(req.params)
+  //console.log(req.headers)
+
+  token = process.env.WEATHER_TOKEN || 'key'
+
+  Signture = require('crypto').createHmac('sha256', token).update(req.params.data).digest().toString('hex');
+
+  // Signture === req.headers['x_signture'] ? null : console.log(req.params)
+
+  if (Signture === req.headers['x_signture']) {
+
+    const data = req.params.data.split(/[A-Z]/)
+    w = new weather({
+      wind_direction_AD: data[1],
+      wind_direction: data[2],
+      wind_speed_Hz: data[3],
+      wind_speed: data[4],
+      wind_speed_before_1m: data[5],
+      wind_speed_before_5m: data[6],
+      rainfall_count: data[7],
+      rainfall_count_before_1m: data[8],
+      rainfall_before_1m: data[9],
+      rainfall_before_1h: data[10],
+      rainfall_before_1d: data[11],
+      temperature: data[12],
+      humidity: data[13],
+      air_pressure: data[14],
+      timestamp: Date.now()
+    })
+
+    //console.log("write database")
+
+    w.save(function (err, d) {
+      if (err) {
+        //logger.error(err);
+        //res.send(`{"code": 0, "msg": "Error happens, please contact system administrator."}`);
+        res.json({ msg: "Error happens, please contact system administrator." })
+      }
+      else {
+        console.log("save successed")
+        res.json({ msg: "save successed" })
+      }
+    })
+  } else {
+    res.json({ msg: "token error" })
+  }
 }
--- toserver.rb
+++ toserver.rb
@@ -1,20 +1,37 @@
 require 'socket'
 require 'net/http'
+require 'openssl'
 
 remote = ARGV[0] || "localhost"
+assess_token = ARGV[1] || "key"
 
 socket = TCPSocket.new 'localhost', 8900
 
+#line = 'A0789B000C0000D0000E0000F0000G0000H0000I0000J0000K0000L0209M703N10233'
+#mac = OpenSSL::HMAC.hexdigest("SHA256", assess_token, line)
+#http = Net::HTTP.new(remote, 1207)
+#http.use_ssl = false
+#res = http.get("/put/#{line}", { x_signture: mac })
+
+#puts res.body
+#puts mac
+
 while line = socket.gets.delete("\r\n")
-  puts line if line.length == 72
+#  puts line if line.length == 72
+  if line.length == 72
+    mac = OpenSSL::HMAC.hexdigest("SHA256", assess_token, line)
+    http = Net::HTTP.new(remote, 1207)
+    http.use_ssl = false
 
-  begin
-    Net::HTTP.get(remote, "/put/#{line}", '1207') if line.length == 72
-  rescue Exception => e
-    puts e.message
-    puts e.backtrace.inspect
+    begin
+      res = http.get("/put/#{line}", { x_signture: mac })
+      #puts res.body
+#      Net::HTTP.get(remote, "/put/#{line}", '1207') if line.length == 72
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+    end
   end
-
 end
 
 s.close
